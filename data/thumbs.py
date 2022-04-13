c6ba07afe669a271a0d4cc8cf00e4a0d5241c613
Sreyeesh Garimella <40808432+Sreyeesh@users.noreply.github.com>
2022-04-13 12:40:34 +0300

    thumbanils is done

--- data/thumbs.py
+++ data/thumbs.py
@@ -1,9 +1,9 @@
 from conf import SAMPLE_INPUTS, SAMPLE_OUTPUTS
 from moviepy.editor import *
 from PIL import Image
+
 original_path = os.path.join(SAMPLE_INPUTS, '/Users/sreyeeshgarimella/Documents/videoConvertor/videoConvertor/data/samples/inputs/sample.mp4')
 thumbnail_directory = os.path.join(SAMPLE_OUTPUTS, "/Users/sreyeeshgarimella/Documents/videoConvertor/videoConvertor/data/samples/thumbnails")
-os.makedirs(thumbnail_directory)
 print(original_path)
 
 clip = VideoFileClip(original_path)
@@ -14,8 +14,11 @@ print("duration",clip.duration) # seconds
 frame_duration = clip.duration
 max_duration = int(frame_duration) + 1
 for i in range(0,max_duration):
-    print(f"frame at {i} seconds")
-    frames = clip.get_frame(i)
-    print(frames) 
-    new_image = Image.fromarray(frames)
-    new_image.save()
+    for i in range(0, max_duration):
+        frame_duration = clip.get_frame(i)
+    # print(frame) # np.array numpy array # inference
+    new_img_filepath = os.path.join(thumbnail_directory, f"{i}.jpg")
+    # print(f"frame at {i} seconds saved at {new_img_filepath}")
+    new_img = Image.fromarray(frame_duration)
+    new_img.save(new_img_filepath)
+    
